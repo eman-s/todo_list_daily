9401b84aa22f223b848a4db4eb583b51d41f7bf1
Emmanuel Salicrup <emmanuel_salicrup@live.com>
2017-07-26 13:55:16 -0400

    added files and config packages

---
+++ todolistapp.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+var app = express();
+//configure mustache with express
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+
+//Allows public folder to be served statically to browsers
+app.use(express.static('public'));
+
+//config body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//config Express Validator
+app.use(expressValidator());
